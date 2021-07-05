06cb196cffb71bbe9e5c7c0048194a870b960f65
celine.roure <celine.roure.pro@gmail.com>
2021-07-05 10:39:40 +1000

    discogs project for heroku deployment

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,62 @@
+class BandsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :set_band, only: [:show, :destroy, :edit, :update]
+  before_action :set_genres, only: [:new, :edit]
+  
+  
+  def index
+    @bands = Band.all.includes(:genre)
+  end
+
+  def show
+  end
+
+  def new
+    @band = Band.new
+  end
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      redirect_to bands_path
+    else
+      set_genres
+      render "new"
+    end
+  end
+
+  def destroy
+    @band.destroy 
+    redirect_to bands_path
+  end
+  
+  def edit
+  end
+
+  def update
+    if @band.update(band_params)
+      redirect_to bands_path
+    else
+      set_genres
+      render "edit"
+    end
+  end
+  
+  private
+  
+  def band_params
+    params.require(:band).permit(:name, :genre_id, :description)
+  end
+
+  def set_genres
+    @genres = Genre.all
+  end
+
+  def set_band
+    begin
+      @band = Band.find(params[:id])
+    rescue
+      render "not_found"
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root "pages#home"
+  get "bands", to: "bands#index", as: "bands"
+  post "bands", to: "bands#create"
+  get "bands/new", to: "bands#new", as: "new_band"
+  get "bands/:id", to: "bands#show", as: "band"
+  delete "bands/:id", to: "bands#destroy"
+  put "bands/:id", to: "bands#update"
+  get "bands/:id/edit", to: "bands#edit", as: "edit_band"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
