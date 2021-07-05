2186f4649699f740fbe58248114c7a72d3601be2
celine.roure <celine.roure.pro@gmail.com>
2021-07-05 12:02:26 +1000

    adds navbar, refactors forms

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "bands/new", to: "bands#new", as: "new_band"
   get "bands/:id", to: "bands#show", as: "band"
   delete "bands/:id", to: "bands#destroy"
-  put "bands/:id", to: "bands#update"
+  patch "bands/:id", to: "bands#update"
   get "bands/:id/edit", to: "bands#edit", as: "edit_band"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
